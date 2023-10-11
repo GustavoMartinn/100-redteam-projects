a0e50fbdec8f0508c66ca435bc4f949dc55ed24c
GustavoMartinn <gumartin29@gmail.com>
2023-10-11 15:27:10 -0300

    feat: add project 2 client

--- Projects/1/client.c
+++ Projects/1/client.c
@@ -3,25 +3,25 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
- 
-int main(){
- 
-  char *ip = "127.0.0.1";
+
+int main() {
+
+  char* ip = "127.0.0.1";
   int port = 1234;
- 
+
   int sock;
   struct sockaddr_in addr;
   socklen_t addr_size;
   char buffer[1024];
   int n;
- 
+
   sock = socket(AF_INET, SOCK_STREAM, 0);
-  if (sock < 0){
+  if (sock < 0) {
     perror("[-]Socket error");
     exit(1);
   }
   printf("[+]TCP server socket created.\n");
- 
+
   memset(&addr, '\0', sizeof(addr));
   addr.sin_family = AF_INET;
   addr.sin_port = port;
@@ -39,12 +39,9 @@ int main(){
   bzero(buffer, 1024);
   recv(sock, buffer, sizeof(buffer), 0);
   printf("Server: %s\n", buffer);
- 
+
   close(sock);
   printf("Disconnected from the server.\n");
- 
- 
- 
- 
+
   return 0;
 }
